0901b166c91f9f1c307b3bc19bc1e46e321453c9
MUHAMMADIBRAR12 <kindom123456987321@gmail.com>
2024-04-01 04:03:55 -0700

    main

--- app/Http/Controllers/webControllers/IndividualBusinessController.php
+++ app/Http/Controllers/webControllers/IndividualBusinessController.php
@@ -1,8 +1,10 @@
 <?php
+namespace App\Http\Controllers\webControllers;
 
-namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\DB;
 
 class IndividualBusinessController extends Controller
 {
